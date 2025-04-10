f8266e6e01a44e570d583499f2002d8b6d17a245
Brian Polo <poloo05@protonmail.com>
2025-04-10 15:41:56 +0200

    helloword.py

--- 01_Day_Introduction/helloword.py
+++ 01_Day_Introduction/helloword.py
@@ -1,1 +1,14 @@
-#
+"""Level 1"""
+#Operands
+
+print(3 + 4) #addition(+)
+print(4 - 3) #substraction(-)
+print(4 % 3) #modulus(%)
+print(4 / 3) #division(/)
+print(3 ** 4) #exponential(**)
+print(4 // 3) #integer division(//)
+
+#Strings. To create a string, just put any character (or text) between quotes.
+
+print("Spain")
+print("I am enjoying 30 days python")
